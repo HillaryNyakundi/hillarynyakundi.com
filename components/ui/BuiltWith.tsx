2ba3c49ba8f62f921ecc1e3a7573b9fbe4c9206c
Benson Macharia <muchokibenson12@gmail.com>
2024-08-06 09:37:12 +0300

    Modified Blog Tags

--- components/ui/BuiltWith.tsx
+++ components/ui/BuiltWith.tsx
@@ -8,13 +8,13 @@ const BuiltWith = () => (
     <span className="mr-1 text-gray-500 dark:text-gray-400">Built with</span>
 
     <div className="flex space-x-1.5">
-      <Link href="https://nextjs.org?ref=karhdo.dev">
+      <Link href="https://nextjs.org?ref=bmacharia.com">
         <BrandIcon type="NextJS" className="h-5 w-5" />
       </Link>
-      <Link href="https://tailwindcss.com?ref=karhdo.dev">
+      <Link href="https://tailwindcss.com?ref=bmacharia.com">
         <BrandIcon type="TailwindCSS" className="h-5 w-5" />
       </Link>
-      <Link href="https://www.typescriptlang.org?ref=karhdo.dev">
+      <Link href="https://www.typescriptlang.org?ref=bmacharia.com">
         <BrandIcon type="Typescript" className="h-5 w-5" />
       </Link>
     </div>
